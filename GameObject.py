8f13e9562a56ad90a62b0961bed64e85f5de43f3
zukarusan <muhammadnabilasykar34@gmail.com>
2020-04-24 15:09:05 +0700

    AND OR operation toward bytes and others

--- GameObject.py
+++ GameObject.py
@@ -1,4 +1,5 @@
 import pyglet
+from enum import Enum
 
 
 class Color:
@@ -14,6 +15,56 @@ class Point:
         self.y = y
 
 
+def ANDColor(color1, color2):
+    clr = Color(0,0,0)
+    clr.r = color1.r & color2.r
+    clr.g = color1.g & color2.g
+    clr.b = color1.b & color2.b
+    return clr
+
+def ANDwPixelByte(pxlb_1, pxlb_2):
+    pxlb = bytes()
+    if len(pxlb_1) <= len(pxlb_2):
+        for i in range(len(pxlb_1)):
+            pxlb += bytes([pxlb_1[i] & pxlb_2[i]])
+    else:
+        for i in range(len(pxlb_2)):
+            pxlb += bytes([pxlb_2[i] & pxlb_1[i]])
+    return pxlb
+
+def ORColor(color1, color2):
+    clr = Color(0,0,0)
+    clr.r = color1.r | color2.r
+    clr.g = color1.g | color2.g
+    clr.b = color1.b | color2.b
+    return clr
+
+def ORwPixelByte(pxlb_1, pxlb_2):
+    pxlb = bytes()
+    if len(pxlb_1) <= len(pxlb_2):
+        for i in range(len(pxlb_1)):
+            pxlb += bytes([pxlb_1[i] | pxlb_2[i]])
+    else:
+        for i in range(len(pxlb_2)):
+            pxlb += bytes([pxlb_2[i] | pxlb_1[i]])
+    return pxlb
+
+class SpongeState(Enum):
+    idle = 0
+    walk = 1
+    leapBegin = 10
+    leap = 11
+    leapEnd = 12
+    throwHrzBegin = 200
+    throwHrz = 201
+    throwHrzEnd = 202
+    spinBegin = 100
+    spin = 101
+    spinEnd = 102
+    faceLeft = 5
+    faceRight = 6
+
+
 class ImageObject:
     def __init__(self, path):
         self.path = path
@@ -23,7 +74,6 @@ class ImageObject:
             self.height = img.height
             self.pitch = -self.width * len('RGB')
             self.data = img.get_data(fmt='RGB', pitch=self.pitch)
-
         except FileNotFoundError:
             print("File not succesfully loaded")
             exit()
@@ -47,6 +97,25 @@ class ImageObject:
                 idx += 3
         return clr
 
+    def get_pixel_bytes(self, x, y, width=None, height=None):
+        if x >= self.width:
+            x = self.width - 1
+        if y >= self.height:
+            y = self.height - 1
+        idx = y * self.width + x
+        idx *= 3
+        if width is None and height is None:
+            return bytes([self.data[idx], self.data[idx + 1], self.data[idx + 2]])
+        pxl = bytes()
+        for j in range(height):
+            for i in range(width):
+                try:
+                    pxl += bytes([self.data[idx], self.data[idx + 1], self.data[idx + 2]])
+                except IndexError:
+                    return pxl
+                idx += 3
+        return pxl
+
     def set_color(self, x, y, color, width=None, height=None):
         if x >= self.width:
             x = self.width - 1
@@ -55,16 +124,16 @@ class ImageObject:
         idx = y * self.width + x
         idx *= 3
         if width is None and height is None:
-            self.data[idx] = color.r
-            self.data[idx + 1] = color.g
-            self.data[idx + 2] = color.b
+            self.data[idx] = bytes([color.r])
+            self.data[idx + 1] = bytes([color.g])
+            self.data[idx + 2] = bytes([color.b])
             return
         for j in range(height):
             for i in range(width):
                 try:
-                    self.data[idx] = color.r
-                    self.data[idx + 1] = color.g
-                    self.data[idx + 2] = color.b
+                    self.data[idx] = bytes([color.r])
+                    self.data[idx + 1] = bytes([color.g])
+                    self.data[idx + 2] = bytes([color.b])
                 except IndexError:
                     return
                 idx += 3
