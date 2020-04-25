588a1e9ae3fbb4582cd490c1db72a9f420dc47ae
zukarusan <muhammadnabilasykar34@gmail.com>
2020-04-26 00:20:26 +0700

    re-typing bytes to bytearray, added initializing in main.py, added get_pixel_bytearray

--- GameObject.py
+++ GameObject.py
@@ -37,20 +37,20 @@ def ANDColor(color1, color2):
 
 def ANDwPixelByte(pxlb_1, pxlb_2):
     '''
-    Bitwise operation antara dua data pixel (bisa dibilang warna) dalam bentuk bytes. Data bisa lebih dari satu pixel
-    yang dicompare. Contoh: satu string bytes yang mempresentasikan sebuah gambar atau garis, di bitwise dengan pixel
+    Bitwise operation antara dua data pixel (bisa dibilang warna) dalam bentuk bytearray. Data bisa lebih dari satu pixel
+    yang dicompare. Contoh: satu string bytearray yang mempresentasikan sebuah gambar atau garis, di bitwise dengan pixel
     byte lainnya tergantung size siapa yang lebih kecil.
-    :param pxlb_1: data bytes pixel RBB order. Bisa lebih dari satu.
-    :param pxlb_2: data bytes pixel RGB order yang ingin di bitwise. Bisa lebih dari satu.
-    :return: satu data pixel (3 bytes) jika kedua param cuma satu pixel juga. > 3 bytes jika banyak.
+    :param pxlb_1: data bytearray pixel RBB order. Bisa lebih dari satu.
+    :param pxlb_2: data bytearray pixel RGB order yang ingin di bitwise. Bisa lebih dari satu.
+    :return: satu data pixel (3 bytearray) jika kedua param cuma satu pixel juga. > 3 bytearray jika banyak.
     '''
-    pxlb = bytes()
+    pxlb = bytearray()
     if len(pxlb_1) <= len(pxlb_2):
         for i in range(len(pxlb_1)):
-            pxlb += bytes([pxlb_1[i] & pxlb_2[i]])
+            pxlb += bytearray([pxlb_1[i] & pxlb_2[i]])
     else:
         for i in range(len(pxlb_2)):
-            pxlb += bytes([pxlb_2[i] & pxlb_1[i]])
+            pxlb += bytearray([pxlb_2[i] & pxlb_1[i]])
     return pxlb
 
 
@@ -70,20 +70,20 @@ def ORColor(color1, color2):
 
 def ORwPixelByte(pxlb_1, pxlb_2):
     '''
-    Bitwise operation antara dua data pixel (bisa dibilang warna) dalam bentuk bytes. Data bisa lebih dari satu pixel
-    yang dicompare. Contoh: satu string bytes yang mempresentasikan sebuah gambar atau garis, di bitwise dengan pixel
+    Bitwise operation antara dua data pixel (bisa dibilang warna) dalam bentuk bytearray. Data bisa lebih dari satu pixel
+    yang dicompare. Contoh: satu string bytearray yang mempresentasikan sebuah gambar atau garis, di bitwise dengan pixel
     byte lainnya tergantung size siapa yang lebih kecil.
-    :param pxlb_1: data bytes pixel RBB order. Bisa lebih dari satu.
-    :param pxlb_2: data bytes pixel RGB order yang ingin di bitwise. Bisa lebih dari satu.
-    :return: satu data pixel (3 bytes) jika kedua param cuma satu pixel juga. > 3 bytes jika banyak.
+    :param pxlb_1: data bytearray pixel RBB order. Bisa lebih dari satu.
+    :param pxlb_2: data bytearray pixel RGB order yang ingin di bitwise. Bisa lebih dari satu.
+    :return: satu data pixel (3 bytearray) jika kedua param cuma satu pixel juga. > 3 bytearray jika banyak.
     '''
-    pxlb = bytes()
+    pxlb = bytearray()
     if len(pxlb_1) <= len(pxlb_2):
         for i in range(len(pxlb_1)):
-            pxlb += bytes([pxlb_1[i] | pxlb_2[i]])
+            pxlb += bytearray([pxlb_1[i] | pxlb_2[i]])
     else:
         for i in range(len(pxlb_2)):
-            pxlb += bytes([pxlb_2[i] | pxlb_1[i]])
+            pxlb += bytearray([pxlb_2[i] | pxlb_1[i]])
     return pxlb
 
 
@@ -93,9 +93,9 @@ class CharState(Enum):
     '''
     idle = 0
     walk = 1
-    leapBegin = 10
-    leap = 11
-    leapEnd = 12
+    jumpBegin = 10
+    jump = 11
+    jumpEnd = 12
 
 
 class DummyState(Enum):
@@ -110,7 +110,6 @@ class SpongeState(Enum):
     State khusus untuk Wire Sponge
     '''
     idle = 0
-    walk = 1
     leapBegin = 10
     leap = 11
     leapEnd = 12
@@ -146,12 +145,15 @@ class ImageObject:
             self.width = img.width
             self.height = img.height
             self.pitch = -self.width * len('RGB')  # Number of bytes per row
-            self.data = img.get_data(fmt='RGB', pitch=self.pitch)  # Pixels bytes data
+            self.data = bytearray(img.get_data(fmt='RGB', pitch=self.pitch))  # Pixels bytearray data
         except FileNotFoundError:
             print("File not succesfully loaded")
             exit()
 
-    def get_color(self, x, y, width=0, height=0):
+    def get_bytes(self):
+        return bytes(self.data)
+
+    def get_color(self, x, y, width=0, height=0) -> Color:
         '''
         Ambil color dengan koordinat image yang ditentukan. Kalau ingin mengambil dalam satu area kotak/garis, width dan
         height harus ditentukan
@@ -179,15 +181,15 @@ class ImageObject:
                 idx += 3
         return clr
 
-    def get_pixel_bytes(self, x, y, width=0, height=0):
+    def get_pixel_bytearray(self, x, y, width=0, height=0) -> bytearray:
         '''
-        Ambil color dalam bentuk bytes of pixels dengan koordinat image yang ditentukan. Kalau ingin mengambil dalam
+        Ambil color dalam bentuk bytearray of pixels dengan koordinat image yang ditentukan. Kalau ingin mengambil dalam
         satu area kotak/garis, width dan height harus ditentukan
         :param x: koordinat x dari LEFT
         :param y: koordinat y dari TOP
         :param width: panjang area jika perlu satu Kotak (Rectangle)
         :param height: lebar area jika perlu satu Kotak (Rectangle)
-        :return: string bytes of pixel (size-nya kelipatan 3, RGB)
+        :return: string bytearray of pixel (size-nya kelipatan 3, RGB)
         '''
         if x >= self.width:
             x = self.width - 1
@@ -196,18 +198,18 @@ class ImageObject:
         idx = y * self.width + x
         idx *= 3
         if width == 0 and height == 0:
-            return bytes([self.data[idx], self.data[idx + 1], self.data[idx + 2]])
-        pxl = bytes()
+            return bytearray([self.data[idx], self.data[idx + 1], self.data[idx + 2]])
+        pxl = bytearray()
         for j in range(height):
             for i in range(width):
                 try:
-                    pxl += bytes([self.data[idx], self.data[idx + 1], self.data[idx + 2]])
+                    pxl += bytearray([self.data[idx], self.data[idx + 1], self.data[idx + 2]])
                 except IndexError:
                     return pxl
                 idx += 3
         return pxl
 
-    def set_color(self, x, y, color, width=0, height=0):
+    def set_color(self, x, y, color: Color, width=0, height=0):
         '''
         Taruh warna di koordinat yang ditentukan. Bisa mengubah satu area yang ditentukan width dan height nya
         :param x: koordinat x dari LEFT
@@ -224,26 +226,42 @@ class ImageObject:
         idx = y * self.width + x
         idx *= 3
         if width == 0 and height == 0:
-            self.data[idx] = bytes([color.r])
-            self.data[idx + 1] = bytes([color.g])
-            self.data[idx + 2] = bytes([color.b])
+            self.data[idx] = bytearray([color.r])
+            self.data[idx + 1] = bytearray([color.g])
+            self.data[idx + 2] = bytearray([color.b])
             return
         for j in range(height):
             for i in range(width):
                 try:
-                    self.data[idx] = bytes([color.r])
-                    self.data[idx + 1] = bytes([color.g])
-                    self.data[idx + 2] = bytes([color.b])
+                    self.data[idx] = bytearray([color.r])
+                    self.data[idx + 1] = bytearray([color.g])
+                    self.data[idx + 2] = bytearray([color.b])
                 except IndexError:
                     return
                 idx += 3
+
+    def set_pixel_bytearray(self, x, y, pixel_byte: bytearray):
+        '''
+        Set a single color in different way. Terdiri dari 3 byte RGB
+        :param x: koordinat x dari LEFT
+        :param y: koordinat y dari TOP
+        :param pixel_byte: color yang berupa 3 bytearray of pixel, RGB
+        :return: nai desu, tehepero
+        '''
+        if x >= self.width:
+            x = self.width - 1
+        if y >= self.height:
+            y = self.height - 1
+        idx = y * self.width + x
+        idx *= 3
+        self.data[idx:idx+3] = pixel_byte
 
 
 class Frame:
     '''
     Frame bertindak seperti grid yang memotong dan memisahkan pada bagian image untuk sprite yang akan di apply.
     '''
-    def __init__(self, xLeft, xRight, yTop, yBottom, anchor, time_framing):
+    def __init__(self, xLeft, xRight, yTop, yBottom, anchor: Vector, time_framing):
         '''
 
         :param xLeft:
@@ -266,9 +284,14 @@ class FrameCollection:
     '''
     Bertindak sebagai penampung Frames. Dipakai sebagai set frame karena akan dianimasikan sesuai state karakternya
     '''
-    def __init__(self):
+    def __init__(self, state):
+        '''
+        Buat set of frames bare, atau bisa dibilang sprites.
+        :param state: state sesuai set of frames nya. Sesuai class state yang di enum kan
+        '''
         self.frames = []
         self.num_frame = len(self.frames)
+        self.state = state
 
     def insert(self, frame):
         '''
@@ -286,34 +309,44 @@ class Character:
         self.velocity = Vector(0,0)
         self.curState = CharState.idle
         self.frameId = 0
-        self.curFrame = 0
+        self.curTimeFrame = 0
         self.sprites = []       # Frame collection
         self.spritesId = 0
-        self.facing = Facing.right
+        self.facing = Facing.left
+        self.getIdSpr = {}
 
-    def setState(self, state, sprId):
+    def insert(self, sprite):
+        self.getIdSpr[sprite.state] = len(self.sprites)
+        self.sprites.append(sprite)
+
+    def setState(self, state):
         self.curState = state
-        self.spritesId = sprId
-        self.curFrame = 0
+        self.spritesId = self.getIdSpr[state]
+        self.curTimeFrame = 0
         self.frameId = 0
 
     def nextFrame(self):
-        self.curFrame += 1
-        if self.curFrame == self.sprites[self.spritesId].frames[self.frameId].framing_time:
+        self.curTimeFrame += 1
+        if self.curTimeFrame == self.sprites[self.spritesId].frames[self.frameId].time_framing:
             self.frameId += 1
             if self.frameId == self.sprites[self.spritesId].num_frame:
                 self.frameId = 0
-            self.curFrame = 0
+            self.curTimeFrame = 0
 
     def update(self):
         pass  # ini untuk karakter biasa/dummy/whatever, disini buat skema nya sesuai finite machine (FSM) karakternya
 
 
 class WireSponge(Character):
-    def __init__(self, position):
+    def __init__(self, position, facing=Facing.left):
         super().__init__(position)
         self.curState = SpongeState.idle
+        self.facing = facing
 
     def update(self):
-        pass # disini buat sesuai FSM karakter si sponge nya
-
+        # disini buat sesuai FSM karakter si sponge nya
+        if self.curState == SpongeState.idle:
+            self.nextFrame()
+            self.setState(SpongeState.idle)
+            self.velocity.x = 0
+            self.velocity.y = 0
--- main.py
+++ main.py
@@ -1,19 +1,82 @@
-import pyglet
+from GameObject import *
+
+bg = ImageObject("spongy_wired.jpg")
+sprsht = ImageObject("spritesheet.png")
+sprmask = ImageObject("spritesheet_mask.png")
+wire_sponge = WireSponge(Vector(800, 480))
+img_draw = bg
+img_display = pyglet.image.ImageData
+
+
+def initialize_sprite():
+    SpongeIdle = FrameCollection(SpongeState.idle)
+    SpongeIdle.insert(
+        Frame(
+            83, 136, 27, 84,
+            Vector(110, 57),
+            1
+        )
+    )
+    wire_sponge.insert(SpongeIdle)
+
+
+def put_sprite(character):
+    img_draw = bg
+    frame = character.sprites[character.spritesId].frames[character.frameId]
+    sprite_width = frame.xRight - frame.xLeft
+    sprite_height = frame.yBottom - frame.yTop
+
+    if character.facing.left:
+        spriteLeft = character.position.x - frame.anchor.x + frame.xLeft
+        spriteTop = character.position.y - frame.anchor.y + frame.yTop
+
+        for i in range(sprite_width):
+            for j in range(sprite_height):
+                img_draw.set_pixel_bytearray(
+                    spriteLeft+i, spriteTop+j,
+                    ANDwPixelByte(
+                        img_draw.get_pixel_bytearray(spriteLeft+i, spriteTop+j),
+                        sprmask.get_pixel_bytearray(frame.xLeft+i, frame.yTop+j)
+                    )
+                )
+
+        for i in range(sprite_width):
+            for j in range(sprite_height):
+                img_draw.set_pixel_bytearray(
+                    spriteLeft+i, spriteTop+j,
+                    ORwPixelByte(
+                        img_draw.get_pixel_bytearray(spriteLeft+i, spriteTop+j),
+                        sprsht.get_pixel_bytearray(frame.xLeft+i, frame.yTop+j)
+                    )
+                )
+
+
+def display_img():
+    put_sprite(wire_sponge)
+    img_display = pyglet.image.ImageData(
+        img_draw.width,
+        img_draw.height,
+        'RGB',
+        img_draw.get_bytes(),
+        img_draw.pitch
+    )
+    img_display.blit(0,0)
+
+
+def update(dt):
+    wire_sponge.update()
+    display_img()
 
 
 class CGA(pyglet.window.Window):
     def __init__(self):
-        super().__init__(width=512, height=512, visible=True)
-        self.label = pyglet.text.Label('Hello, world',
-                                       font_name='Times New Roman',
-                                       font_size=36,
-                                       x=self.width // 2, y=self.height // 2,
-                                       anchor_x='center', anchor_y='center')
+        super().__init__(width=989, height=640, visible=True)
 
     def on_draw(self):
         self.clear()
-        self.label.draw()
 
 
+initialize_sprite()
+pyglet.clock.schedule_interval(update, 0.5)
 cga = CGA()
 pyglet.app.run()
